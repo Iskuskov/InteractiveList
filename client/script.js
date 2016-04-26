448dc16c90e66b57bde076dbe3fc30b09e446b50
Iskuskov Alexander <iskuskov@gmail.com>
2016-04-26 15:52:15 +0300

    [add] Step 4: Drag'n'Drop

--- client/script.js
+++ client/script.js
@@ -1,9 +1,21 @@
-var generateElement = function(index, text) {
+var addElement = function(index, text) {
 	var parent = $("#list");
 	var wrapper = $("<li />", {
 		"id" : index,
-		"text" : text
+		"text" : text,
+		"class" : "list"
 	}).appendTo(parent);
+};
+
+var moveElement = function(startPos, stopPos) {
+	var startLi = $("#list li:eq(" + startPos + ")");
+	var stopLi = $("#list li:eq(" + stopPos + ")");
+	
+	if (startPos < stopPos) {
+		startLi.insertAfter(stopLi);
+	} else {
+		startLi.insertBefore(stopLi);
+	}
 };
 
 $(document).on('ready', function() {
@@ -12,15 +24,41 @@ $(document).on('ready', function() {
 
 	socket.onmessage = function(event) {
 		var message = JSON.parse(event.data);
-		generateElement(message.id, message.text);
+		var action = message.action;
+		
+		if (action == "add") {
+			addElement(message.data.id, message.data.text);
+		} else if (action == "move") {
+			moveElement(message.data.start, message.data.stop);
+		}
 	};
-
+	
 	$("form").on("submit", function(e) {
 		e.preventDefault();
 		var text = $("#adding-input").val();
 		if (text.length == 0) return;
 		$("#adding-input").val('');
-		socket.send(text);
-	})
+		socket.send(JSON.stringify({action: "add", data: text}));
+	});
+	
+	$('#list').sortable({
+		revert: true,
+		placeholder: "highlight",
+		start: function (event, ui) {
+			ui.item.toggleClass("highlight");
+			
+			var start_pos = ui.item.index();
+            ui.item.data('start_pos', start_pos);
+		},
+		stop: function (event, ui) {
+			ui.item.toggleClass("highlight");
+			
+			var start_pos = ui.item.data('start_pos');
+			var stop_pos = ui.item.index();
+			if (start_pos != stop_pos) {
+				var sendData = {start: start_pos, stop: stop_pos};
+				socket.send(JSON.stringify({action: "move", data: sendData}));
+			}
+		}
+	});
 });
-
--- server/server.js
+++ server/server.js
@@ -2,6 +2,14 @@ var WebSocketServer = new require('ws');
 
 var clients = {};
 var data = {};
+var order = [];
+
+function moveElementInArray(array, oldIndex, newIndex) {
+	var arrayClone = array.slice();
+	arrayClone.splice(oldIndex, 1);
+	arrayClone.splice(newIndex, 0, array[oldIndex]);
+    return arrayClone
+}
 
 var webSocketServer = new WebSocketServer.Server({
 	port: 8081
@@ -12,18 +20,38 @@ webSocketServer.on('connection', function(ws) {
 	clients[clientId] = ws;
 	console.log("New connection " + clientId);
 
-	for (var index in data) {
-		ws.send(JSON.stringify({id: index, text: data[index]}));
+	for (var i in order) {
+		var index = order[i];
+		var sendData = {id: index, text: data[index]};
+		ws.send(JSON.stringify({action: "add", data: sendData}));
 	}
 	
 	ws.on('message', function(message) {
 		console.log('Receive message ' + message);
 
-		index = new Date().getTime();
-		data[index] = message;
+		var message = JSON.parse(message);
+		var action = message.action;
 		
-		for (var key in clients) {
-			clients[key].send(JSON.stringify({id: index, text: message}));
+		if (action == "add") {
+			var index = new Date().getTime();
+			var text = message.data;
+			
+			data[index] = text;
+			order.push(index);
+			
+			for (var key in clients) {
+				var sendData = {id: index, text: text};
+				clients[key].send(JSON.stringify({action: "add", data: sendData}));
+			}
+		} else if (action == "move") {
+			
+			order = moveElementInArray(order, message.data.start, message.data.stop);
+			
+			for (var key in clients) {
+				if (key != clientId) {
+					clients[key].send(JSON.stringify({action: "move", data: message.data}));
+				}
+			}
 		}
 	});
 
